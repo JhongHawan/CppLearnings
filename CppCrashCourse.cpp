1726f7d43cad07b804e6d2fed08f82ba48cfd870
brian <brianjhong@outlook.com>
2025-06-05 23:09:03 -0700

    Add loops

--- CppCrashCourse.cpp
+++ CppCrashCourse.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main()
+void main2()
 {
+	bool isTodaySunny = false;
 
+	if (isTodaySunny) {
+		cout << "Go to the park" << endl;
+	}
+	else {
+		cout << "Take an umbrella" << endl;
+	}
+
+	//if (isTodaySunny) : 
+
+	system("pause>0");
 }
--- CppLearnings.cpp
+++ CppLearnings.cpp
@@ -2,16 +2,76 @@
 // Using the std namespace to save time but really you should be using more specific namespace
 using namespace std; 
 
-int main()
-{
+float sum(float a, float b);
+
+void loops() {
+
+    // while loops
+    cout << "WHILE:\n";
+    int counter = 1;
+    while (counter <= 10) {
+        cout << counter << endl;
+        counter++;
+    }
+
+    // do while loops
+    cout << "DO-WHILE:\n";
+    int doWhileCounter = 1;
+
+    do {
+        cout << doWhileCounter << endl;
+        doWhileCounter++;
+    } while (doWhileCounter <= 10);
+
+    // for loops
+    cout << "FOR-LOOP:\n";
+    string animals[5] = { "cat", "dog", "cow", "goat", "bee" };
+    for (int i = 0; i < 5; i++) {
+        cout << animals[i] << endl;
+    }
+}
+
+enum EyeColor {Brown, Blue, Green};
+
+void conditionals() {
+    bool isTodaySunny = false;
+    bool isTodayWeekend = false;
+
+    if (isTodayWeekend && isTodaySunny) {
+        cout << "Go to the park" << endl;
+    }
+    else if (isTodayWeekend && !isTodaySunny) {
+        cout << "Go to the park, but take an umbrella" << endl;
+    }
+    else {
+        cout << "Go to work" << endl;
+    }
+
+    // Ternary/Conditional operator Shorthand for if/else here
+    //isTodaySunny ? cout << "Go to the park" : cout << "Take an umbrella";
+
+    // Switch statements
+    EyeColor eyeColor = Blue;
+
+    switch (eyeColor) {
+        case Brown: cout << "Brown eyes"; break;
+        case Blue: cout << "Blue eyes"; break;
+        case Green: cout << "Green eyes"; break;
+        default:cout << "Not a valid eye color"; break;
+    }
+    
+}
+
 // Region works like C# 
 #pragma region PrimitiveTypes
+
+void types() {
 
     // Constants similar to c#
     const int constantAge = 25;
     int age = 25;
     age = 26;
-    
+
     // Float for storing decimals
     float weight = 65.5;
 
@@ -27,7 +87,7 @@ int main()
     bool isTodaySunny = false;
 
     // Arrays
-    string colors[] = { "red", "green", "pink", "blue", "black" }; 
+    string colors[] = { "red", "green", "pink", "blue", "black" };
     // Arrays with fixed size
     string colors2[10] = {};
 
@@ -37,10 +97,25 @@ int main()
     cin >> colors[0];
     cout << colors[0];
     colors[1] = "gray";
+}
 
 #pragma endregion
+
+/// <summary>
+/// Summary comments work the same as C#
+/// main() has to be declared at the end of the file because the C++ compiler
+/// works from top to bottom when figuring out functions
+/// </summary>
+int main()
+{
+    loops();
+    sum(2.2, 5);
 
     // This command gets rid of the directory and other junk info when running the
     // standard output above.
     system("pause>0");
+}
+
+float sum(float a, float b) {
+    return a + b;
 }
