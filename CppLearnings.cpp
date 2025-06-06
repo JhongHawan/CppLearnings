143aecf66d87c547744365df3de902eee30fafd8
brian <brianjhong@outlook.com>
2025-06-05 22:31:15 -0700

    Go through arrays and primitive types

--- CppLearnings.cpp
+++ CppLearnings.cpp
@@ -4,10 +4,41 @@ using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
-    // std is a namespace to be able to run the cout command 
-    // and others.
-    std::cout << "Welcome to the C++ Crash Course\n";
+// Region works like C# 
+#pragma region PrimitiveTypes
+
+    // Constants similar to c#
+    const int constantAge = 25;
+    int age = 25;
+    age = 26;
+    
+    // Float for storing decimals
+    float weight = 65.5;
+
+    // Double can represent decimal numbers to a higher
+    // precision than float. It's double the capacity as float.
+    double weightDouble = 65.5;
+
+    // Double quotes don't work for char
+    char gender = 'f';
+
+    string username = "CodeBeauty";
+
+    bool isTodaySunny = false;
+
+    // Arrays
+    string colors[] = { "red", "green", "pink", "blue", "black" }; 
+    // Arrays with fixed size
+    string colors2[10] = {};
+
+    // Print single element from the array
+    cout << colors[0];
+    // Accept input from console and store in position 0 of colors array
+    cin >> colors[0];
+    cout << colors[0];
+    colors[1] = "gray";
+
+#pragma endregion
 
     // This command gets rid of the directory and other junk info when running the
     // standard output above.
