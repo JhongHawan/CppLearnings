f9ee895983cd199256a6bbb80e209e51056a71f1
brian <brianjhong@outlook.com>
2025-06-06 12:03:12 -0700

    Add generics

--- CppLearnings.cpp
+++ CppLearnings.cpp
@@ -3,8 +3,28 @@
 using namespace std; 
 
 // Function declaration goes before main 
+#pragma region FunctionDeclarations
+ 
+void conditionals();
+
+void types();
+
 void loops();
+
 float sum(float a, float b);
+
+template<class T>
+//template<typename T> // you can also use the typename instead of class. It's interchangeable 
+// seems to just be a style thing.
+void Swap(T& a, T& b);
+
+#pragma endregion
+
+#pragma region Enums
+
+enum EyeColor { Brown, Blue, Green };
+
+#pragma endregion
 
 /// <summary>
 /// Summary comments work the same as C#
@@ -15,21 +35,74 @@ float sum(float a, float b);
 /// </summary>
 int main()
 {
+    //types();
+    //conditionals();
     //loops();
     // Double is allowed here instead of float and it's interpreted but this 
     // throws a warning so it's better to be explicit. ints are okay though.
-    cout << sum(2.2, 5) << endl;
-    cout << sum(2.2f, 5) << endl;
-    cout << sum(5.2f, 3) << endl;
-    cout << sum(4, 8) << endl;
+    //cout << sum(2.2, 5) << endl;
+    //cout << sum(2.2f, 5) << endl;
+    //cout << sum(5.2f, 3) << endl;
+    //cout << sum(4, 8) << endl;
+
+    // sizeof() is used for figuring out memory usage. Maybe this gets used a lot in C++?
+    //int x = 2;
+    //cout << "Size of int is " << sizeof(x) << " bytes\n";
+
+    int a = 5, b = 7;
+    cout << a << " - " << b << endl;
+    Swap<int>(a, b);
+    cout << a << " - " << b << endl;
+
+    char c = 'c', d = 'd';
+    cout << c << " - " << d << endl;
+    Swap<char>(c, d);
+    cout << c << " - " << d << endl;
 
     // This command gets rid of the directory and other junk info when running the
     // standard output above.
     system("pause>0");
 }
 
-float sum(float a, float b) {
-    return a + b;
+
+/// <summary>
+/// The & here is the reference to the original values.
+/// So if they are modified here their values will change.
+/// 
+/// Alias:
+/// A reference is not a separate variable but another name for an existing one.
+/// 
+/// Direct Modification :
+/// Changes made to a reference parameter inside a function directly affect the original variable passed to the function.
+/// 
+/// No Copying :
+/// Unlike passing by value, passing by reference avoids creating a copy of the argument, which can be more efficient, especially for large objects.
+/// Function Parameter References
+/// 
+/// When a function parameter is declared as a reference using&, it binds to the original argument passed during the function call.
+/// 
+/// Example: 
+/// --------------------------
+/// void modifyValue(int& num) {
+///     num = 100; // Modifies the original variable
+/// }
+/// 
+/// int main() {
+///     int myNum = 10;
+///     std::cout << "Before modification: " << myNum << std::endl; // Output: 10
+///     modifyValue(myNum);
+///     std::cout << "After modification: " << myNum << std::endl; // Output: 100
+///     return 0;
+/// }
+/// --------------------------
+/// 
+/// Generic example here using the template with typename
+/// </summary>
+template<class T>
+void Swap(T& a, T& b) {
+    T temp = a;
+    a = b;
+    b = temp;
 }
 
 void loops() {
@@ -59,7 +132,9 @@ void loops() {
     }
 }
 
-enum EyeColor {Brown, Blue, Green};
+float sum(float a, float b) {
+    return a + b;
+}
 
 void conditionals() {
     bool isTodaySunny = false;
